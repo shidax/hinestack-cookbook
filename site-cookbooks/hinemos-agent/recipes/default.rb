8921aad89bb3c820ef9b1934454c7e3e3b306463
Takahiro Shida <shida.takahiro@gmail.com>
2014-05-25 08:20:17 +0900

    add expect package

--- site-cookbooks/hinemos-agent/recipes/default.rb
+++ site-cookbooks/hinemos-agent/recipes/default.rb
@@ -8,7 +8,7 @@
 #
 
 log "Install or update packages"
-%w{rsyslog net-snmp krb5-workstation}.each do |pkg|
+%w{rsyslog net-snmp krb5-workstation expect}.each do |pkg|
   yum_package pkg do
     arch    'x86_64'
   end
@@ -29,7 +29,7 @@ script "install_hinemos_agent" do
   code <<-EOL
     tar xzvf /tmp/#{filename} -C /tmp/
     cd /tmp/#{extract_name}/
-    bash agent_installer_En.sh -i -m 172.24.4.227
+    sudo bash agent_installer_EN.sh -i -m 172.24.4.227
   EOL
 end
 
