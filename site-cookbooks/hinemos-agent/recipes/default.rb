49baed9b696302118027f2c98cd84710c51e111a
Takahiro Shida <shida.takahiro@gmail.com>
2014-05-25 08:25:05 +0900

    remove sudo

--- site-cookbooks/hinemos-agent/recipes/default.rb
+++ site-cookbooks/hinemos-agent/recipes/default.rb
@@ -29,7 +29,7 @@ script "install_hinemos_agent" do
   code <<-EOL
     tar xzvf /tmp/#{filename} -C /tmp/
     cd /tmp/#{extract_name}/
-    sudo bash agent_installer_EN.sh -i -m 172.24.4.227
+    bash agent_installer_EN.sh -i -m 172.24.4.227
   EOL
 end
 
