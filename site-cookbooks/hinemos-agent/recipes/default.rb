8c84bb26a81a0cbfe702c919f6f72c1ba5951933
Takahiro Shida <shida.takahiro@gmail.com>
2014-05-25 07:56:23 +0900

    add package

--- site-cookbooks/hinemos-agent/recipes/default.rb
+++ site-cookbooks/hinemos-agent/recipes/default.rb
@@ -8,7 +8,7 @@
 #
 
 log "Install or update packages"
-%w{rsyslog net-snmp}.each do |pkg|
+%w{rsyslog net-snmp krb5-workstation}.each do |pkg|
   yum_package pkg do
     arch    'x86_64'
   end
@@ -16,6 +16,7 @@ end
 
 log "Retrieve hinemos-agent archive"
 filename = "hinemos_agent-4.1.1_rhel5-rhel6.tar.gz"
+extract_name = "Hinemos_Agent-4.1.1_rhel5-rhel6"
 
 cookbook_file "/tmp/#{filename}" do
   source "#{filename}"
@@ -26,8 +27,8 @@ script "install_hinemos_agent" do
   interpreter "bash"
   user        "root"
   code <<-EOL
-    tar xzvf /tmp/#{filename} -C
-    cd /tmp/#{filename}/
+    tar xzvf /tmp/#{filename} -C /tmp/
+    cd /tmp/#{extract_name}/
     bash agent_installer_all
   EOL
 end
