433f7e855725b8e12968afdbe9c500ca590a58da
Takahiro Shida <shida.takahiro@gmail.com>
2014-05-25 14:59:06 +0900

    Add register code

--- site-cookbooks/hinemos-agent/recipes/default.rb
+++ site-cookbooks/hinemos-agent/recipes/default.rb
@@ -17,6 +17,8 @@ end
 log "Retrieve hinemos-agent archive"
 filename = "hinemos_agent-4.1.1_rhel5-rhel6.tar.gz"
 extract_name = "Hinemos_Agent-4.1.1_rhel5-rhel6"
+register = "hinemos.tgz"
+register_extract_name = "hinemos"
 
 cookbook_file "/tmp/#{filename}" do
   source "#{filename}"
@@ -30,6 +32,22 @@ script "install_hinemos_agent" do
     tar xzvf /tmp/#{filename} -C /tmp/
     cd /tmp/#{extract_name}/
     bash agent_installer_EN.sh -i -m 172.24.4.227
+  EOL
+end
+
+
+log "Register to hinemos manager"
+cookbook_file "/tmp/#{register}" do
+  source "#{register}"
+end
+
+script "register_manager" do
+  interpreter "bash"
+  user        "root"
+  code <<-EOL
+    tar xvzf /tmp/#{register} -C /tmp
+    cd /tmp/#{register_extract_name}
+    python node_regist.py -M 192.168.27.145 -U hinemos -w hinemos -A 172.24.4.227 -n cookbook
   EOL
 end
 
